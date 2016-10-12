d2631dc41ba01600385b6868a00b63b145b847a6
shinji <rill0131@gmail.com>
2016-10-13 01:30:47 +0900

    fix sass.js for outputting to dist

--- gulp/sass.js
+++ gulp/sass.js
@@ -15,12 +15,17 @@ var prefixerOptions = {
 	browsers: ['Android 4.0']
 }
 
+var sassPath = {
+	dist : "dist/"
+}
+
 gulp.task("sass", function() {
     gulp.src(["**/*.scss","!./node_modules/**"])
     .pipe(rename(function(path){
     	
-    		path.dirname = path.dirname.replace("sass","styles");
-    	
+    	path.dirname = path.dirname.replace("sass","styles");
+    	path.dirname = path.dirname.replace("src/",sassPath.dist);
+  
     	return path 
     }))
     	.pipe(plumber())
